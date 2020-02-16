03919259e522f3e230980398bb112866ccef27b5
Anatolii Padenko <tolik.padenko@gmail.com>
2020-02-15 18:48:15 -0800

    add maps searhc api endpoint

--- core-api-service/GretaFoodCore/Controllers/MapController.cs
+++ core-api-service/GretaFoodCore/Controllers/MapController.cs
@@ -9,19 +9,28 @@ namespace GretaFoodCore.Api.Controllers
     [Route("api/[controller]")]
     public class MapController
     {
+        private string _tomTomApiKey;
+        public MapController(CommandLineArguments options)
+        {
+            _tomTomApiKey = options.TomTomApiKey;
+        }
         
-        // [HttpGet("{searchString}")]
-        // public async Task<TomTomResponse> SearchInSF(string searchString)
-        // {
-        //     var client = new RestClient("https://api.tomtom.com");
-        //     var response = await client.ExecuteTaskAsync<TomTomResponse>(request);
-        //
-        //     return response.Data;
-        //     //return  _gretaFoodDb.Restaurants.ToList();
-        // }
-        // public void GetAllRestaurants()
-        // {
-        //     return  _gretaFoodDb.Restaurants.ToList();
-        // }
+        [HttpGet("{searchString}")]
+        public async Task<TomTomResponse> SearchInSF(string searchString)
+        {
+            var client = new RestClient("https://api.tomtom.com");
+            var request = new RestRequest($"search/2/search/{searchString}.json");
+            request.AddQueryParameter("countrySet", "US");
+            //San Fran
+            request.AddQueryParameter("lat", "37.783676");
+            request.AddQueryParameter("lon", "-122.412736");
+
+            request.AddQueryParameter("radius", "50000");
+            request.AddQueryParameter("key", _tomTomApiKey);
+
+            var response = await client.ExecuteTaskAsync<TomTomResponse>(request);
+        
+            return response.Data;
+        }
     }
 }
